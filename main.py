0061aa13657f53c76458db7448b2bbeb5cd22673
Ichinoe <shiinahiiragi@outlook.com>
2024-10-15 16:27:34 +0800

    add new dir

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import sys
 
 sys.dont_write_bytecode = True
-from chimerax import ChimeraX, ChimeraXTask
+from ChimeraX.chimerax import ChimeraX, ChimeraXTask
 
 if __name__ == "__main__":
     with ChimeraX(sort="daily", port=8080, gui=True, version="0.4") as chimerax:
-        single_task = ChimeraXTask(config_path="task.json", manager=chimerax)
+        single_task = ChimeraXTask(config_path="ChimeraX/task.json", manager=chimerax)
         print(single_task.test())
