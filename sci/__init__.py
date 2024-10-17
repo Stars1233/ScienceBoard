c9d0f0b6d16b8cc35ead8f806e1c39ec4a782c5f
Ichinoe <shiinahiiragi@outlook.com>
2024-10-17 18:07:10 +0800

    move log

--- sci/__init__.py
+++ sci/__init__.py
@@ -2,8 +2,9 @@ from .base.agent import Message, Content
 from .base.agent import Access, Overflow
 from .base.agent import Model
 from .base.agent import Agent
+
 from .base.manager import Manager
 from .base.task import Task
+from .base.log import Log
 
-from .log import Log
 from .tester import TaskInfo, Tester
