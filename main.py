7337a564433a7c6394c454b45a0a25b6fd28ae71
Ichinoe <shiinahiiragi@outlook.com>
2024-10-23 11:16:48 +0800

    add presets

--- main.py
+++ main.py
@@ -1,38 +1,22 @@
 import sys
 
 sys.dont_write_bytecode = True
-from sci import Tester, Model, Agent, Prompts
-from sci import ChimeraX
+from sci import Presets, Tester
 
 if __name__ == "__main__":
-    model = Model(
-        style="openai",
+    managers = Presets.managers_spawner()
+    agents = Presets.agents_spawner(
+        model_style="openai",
         base_url="http://server.ichinoe.xyz:500/v1/chat/completions",
         model_name="/mnt/workspace/ichinoe/model/InternVL2-8B/snapshots/357996b2cba121dce8748498968e9fddcc62e386",
+        overflow_style="openai_lmdeploy"
     )
-
-    agent_dict = {
-        "ChimeraXRaw": Agent(
-            model=model,
-            overflow_style="openai_lmdeploy",
-            system_inst=Prompts.SYSTEM_INST_CHIMERAX_RAW
-        )
-    }
-
-    manager_dict = {
-        "ChimeraXRaw": ChimeraX.RawManager(
-            sort="daily",
-            port=8080,
-            gui=True,
-            version="0.4"
-        )
-    }
 
     Tester(
         tasks_path="~/Downloads/tasks",
         logs_path="~/Downloads/logs",
-        agents=agent_dict,
-        managers=manager_dict,
+        agents=agents,
+        managers=managers,
         ignore=False,
         debug=True
     )()
---
+++ sci/Presets.py
@@ -0,0 +1,47 @@
+import sys
+
+from typing import Optional, Dict
+
+sys.dont_write_bytecode = True
+from . import Manager, Model, Agent
+from . import Prompts
+
+from . import ChimeraX
+
+def agents_spawner(
+    model_style: str,
+    base_url: str,
+    model_name: str,
+    api_key: Optional[str] = None,
+    proxy: Optional[str] = None,
+    access_style: str = "openai",
+    code_style: str = "antiquot",
+    overflow_style: Optional[str] = None,
+) -> Dict[str, Agent]:
+    model = Model(
+        style=model_style,
+        base_url=base_url,
+        model_name=model_name,
+        api_key=api_key,
+        proxy=proxy
+    )
+
+    return {
+        "ChimeraXRaw": Agent(
+            model=model,
+            access_style=access_style,
+            code_style=code_style,
+            overflow_style=overflow_style,
+            system_inst=Prompts.SYSTEM_INST_CHIMERAX_RAW
+        )
+    }
+
+def managers_spawner() -> Dict[str, Manager]:
+    return{
+        "ChimeraXRaw": ChimeraX.RawManager(
+            sort="daily",
+            port=8000,
+            gui=True,
+            version="0.4"
+        )
+    }
