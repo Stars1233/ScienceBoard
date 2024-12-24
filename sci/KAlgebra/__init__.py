551cf3c21f162ff9822e06ece9a6f88839e24e9d
Ichinoe <shiinahiiragi@outlook.com>
2024-12-24 17:47:33 +0800

    add startup

--- sci/KAlgebra/__init__.py
+++ sci/KAlgebra/__init__.py
@@ -2,3 +2,4 @@ import sys
 
 sys.dont_write_bytecode
 from .kalgebra import RawManager
+from .task import RawTask
--- sci/KAlgebra/kalgebra.py
+++ sci/KAlgebra/kalgebra.py
@@ -1,24 +1,49 @@
 import sys
+import os
+import subprocess
+
 from typing import Self
 
+import requests
 from PIL import Image
 
 sys.dont_write_bytecode
 from ..base import Manager
 
 class RawManager(Manager):
-    def __init__(self, version: str) -> None:
+    def __init__(
+        self,
+        bin_path: str,
+        lib_path: str,
+        version: str = "0.1"
+    ) -> None:
         super().__init__(version)
+
+        assert os.path.isfile(bin_path)
+        self.bin_path = bin_path
+
+        assert os.path.isdir(lib_path)
+        self.lib_path = lib_path
 
     def __call__(self) -> None:
         raise NotImplementedError
 
     def __enter__(self) -> Self:
-        ...
+        env = os.environ.copy()
+        env["LD_LIBRARY_PATH"] = self.lib_path
+        self.process = subprocess.Popen(
+            self.bin_path,
+            env=env,
+            stdout=subprocess.PIPE,
+            text=True
+        )
+
+        version = requests.get("http://localhost:8080/version").text
+        assert version == self.version
         return super().__enter__()
 
     def __exit__(self, exc_type, exc_value, traceback) -> None:
-        ...
+        self.process.kill()
         return super().__exit__(exc_type, exc_value, traceback)
 
     def screenshot(self) -> Image.Image:
