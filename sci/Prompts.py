1b41e0bc140c37de0348e2e1c88b7ff8cd1ef334
Ichinoe <shiinahiiragi@outlook.com>
2025-01-09 17:20:44 +0800

    fix lean prompts

--- sci/Prompts.py
+++ sci/Prompts.py
@@ -12,15 +12,14 @@ VM_PASSWORD = "password"
 LEAN_IS = "an interactive theorem prover"
 LEAN_RAW_NEED = "JSON of Lean REPL"
 LEAN_RAW_USAGE = [
-    "You will be given a unproved theorem declared by `sorry`, which is what you need to prove."
+    "You will be given a unproved theorem declared by `sorry`, which is what you need to prove.",
     "Each round you will receive the imports information and initial states together with possible historical interaction records as textual observation, and you are expected to apply EXACTLY ONE tactic as a response.",
-    "Your submission should be a JSON with and only with `tactic` and `proofState` fields, such as {\"tactic\": \"apply Int.natAbs\", \"proofState\": 0}`."
+    "Your submission will be passed to REPL directly, and it should be a JSON with and only with `tactic` and `proofState` fields, such as {\"tactic\": \"apply Int.natAbs\", \"proofState\": 0}`.",
     "It is worth noting that the `proofState` which REPL returns contains all proof states stored, so your JSON should also contains this field from which the proof continues."
 ]
 LEAN_RAW_TIPS = [
-    "DO NOT forget to attach a field of proofState in the request object, or your input will be ignored.",
-    "DO NOT use `sorry` "
-    "NEVER submit any Lean 3 code which the compiler of Lean 4 no longer supported."
+    "DO NOT use `sorry` or `admit` to forcibly close the proof, or your input will be ignored.",
+    "NEVER submit any Lean 3 code which the compiler of Lean 4 no longer accpets."
 ]
 
 CHIMERAX_IS = "a molecular visualization software"
