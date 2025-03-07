2e9602bd9170fe2a2217d11aec15d0129081df7d
ichinoe <shiinahiiragi@outlook.com>
2025-03-07 16:16:39 +0800

    add aa

---
+++ tasks/Lean_Raw/abstract_algebra.lean
@@ -0,0 +1,32 @@
+import Mathlib
+
+-- Abstract Algebra: P39, T7
+theorem AA_1
+  [Group G]
+  [IsCyclic G]
+  (N : Subgroup G)
+  [Pow (G ⧸ N) ℤ]
+  : IsCyclic (G ⧸ N)
+  := by sorry
+
+-- Abstract Algebra: P77, T8
+theorem AA_2
+  [Ring R]
+  (N : Ideal R)
+  [DivisionRing (R ⧸ N)]
+  (a : R)
+  : (∃ t : N, t = a * a) → (∃ t' : N, t' = a)
+  := by sorry
+
+-- Abstract Algebra: P110, T4
+theorem AA_3
+  (K E F: Set α)
+  [Field K]
+  [Field E]
+  [Field F]
+  [Algebra F E]
+  [Algebra F K]
+  (p₁ : F ⊆ E) (p₂ : E ⊆ K)
+  (t : Normal F K)
+  : Nonempty (Algebra E K) ∧ ([Algebra E K] → (Normal E K))
+  := by sorry
