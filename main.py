93e4f7912a06a3fef64e83e167da37d053522292
ichinoe <shiinahiiragi@outlook.com>
2025-04-28 19:27:32 +0800

    new config lists

--- main.py
+++ main.py
@@ -3,85 +3,89 @@ import os
 
 sys.dont_write_bytecode = True
 sys.stdout.reconfigure(encoding="utf-8")
-from sci import OBS, Automata, Tester
+from sci import Automata, Tester, OBS
+from sci import AIOAgent, AllInOne
 
-gpt_4o = lambda: Automata(
+gpt_4o = lambda cls: Automata(
     model_style="openai",
     base_url="https://api.openai.com/v1/chat/completions",
     model_name="gpt-4o-2024-08-06",
     api_key=os.environ["OPENAI_API_KEY"],
     proxy=os.environ["HTTPX_PROXY"],
     overflow_style="openai_gpt"
-)
+)(cls)
 
-gpt_o3 = lambda: Automata(
+gpt_o3 = lambda cls: Automata(
     model_style="openai",
     base_url="https://api.openai.com/v1/chat/completions",
     model_name="o3-mini-2025-01-31",
     api_key=os.environ["OPENAI_API_KEY"],
     proxy=os.environ["HTTPX_PROXY"],
     overflow_style="openai_gpt"
-)
+)(cls)
 
-gemini_2 = lambda: Automata(
+gemini_2 = lambda cls: Automata(
     model_style="openai",
     base_url="https://generativelanguage.googleapis.com/v1beta/openai/chat/completions",
     model_name="gemini-2.0-flash-exp",
     api_key=os.environ["GOOGLE_API_KEY"],
     proxy=os.environ["HTTPX_PROXY"],
     overflow_style="openai_gpt"
-)
+)(cls)
 
-claude_3 = lambda: Automata(
+claude_3 = lambda cls: Automata(
     model_style="anthropic",
     base_url="https://api.anthropic.com/v1/messages",
     model_name="claude-3-7-sonnet-20250219",
     api_key=os.environ["ANTHROPIC_API_KEY"],
     proxy=os.environ["HTTPX_PROXY"],
     overflow_style="anthropic"
-)
+)(cls)
 
-qvq = lambda: Automata(
+qvq = lambda cls: Automata(
     model_style="openai",
     base_url=os.environ["QVQ_VL_URL"],
     model_name=os.environ["QVQ_VL_NAME"],
     overflow_style="openai_gpt",
     hide_text=True
-)
+)(cls)
 
-qwen25_vl = lambda: Automata(
+qwen25_vl = lambda cls: Automata(
     model_style="openai",
     base_url=os.environ["QWEN_VL_URL"],
     model_name=os.environ["QWEN_VL_NAME"],
     overflow_style="openai_gpt",
     hide_text=True
-)
+)(cls)
 
-intern_vl = lambda: Automata(
+intern_vl = lambda cls: Automata(
     model_style="openai",
     base_url=os.environ["INTERN_VL_URL"],
     model_name=os.environ["INTERN_VL_NAME"],
     overflow_style="openai_gpt",
     hide_text=True
-)
+)(cls)
 
-tars_dpo = lambda: Automata(
+tars_dpo = lambda cls: Automata(
     model_style="openai",
     base_url=os.environ["TARS_DPO_URL"],
     model_name=os.environ["TARS_DPO_NAME"],
     overflow_style="openai_gpt",
     hide_text=True
-)
+)(cls)
 
 
 # this file somehow acts as a config file
 # with some sensitive contents hidden in env
 if __name__ == "__main__":
+    MODEL = "gpt_4o"
+    AGENT = gpt_4o(AIOAgent)
+
     # register a tester and execute it
     Tester(
         tasks_path="./tasks/Raw",
-        logs_path="./logs/gpt_4o-raw-textual",
-        automata=gpt_4o()
+        logs_path=f"./logs/{MODEL}-raw-textual",
+        community=AllInOne(AGENT)
     )()
 
     # alternative for Tester.__call__()
@@ -89,32 +93,32 @@ if __name__ == "__main__":
     Tester.plan([
         {
             "tasks_path": "./tasks/VM",
-            "logs_path": "./logs/gpt_4o-vm-screenshot",
-            "automata": gpt_4o(),
+            "logs_path": f"./logs/{MODEL}-vm-screenshot",
+            "community": AllInOne(AGENT),
             "vm_path": os.environ["VM_PATH"],
             "headless": True,
             "obs_types": {OBS.screenshot}
         },
         {
             "tasks_path": "./tasks/VM",
-            "logs_path": "./logs/gpt_4o-vm-a11y_tree",
-            "automata": gpt_4o(),
+            "logs_path": f"./logs/{MODEL}-vm-a11y_tree",
+            "community": AllInOne(AGENT),
             "vm_path": os.environ["VM_PATH"],
             "headless": True,
             "obs_types": {OBS.a11y_tree}
         },
         {
             "tasks_path": "./tasks/VM",
-            "logs_path": "./logs/gpt_4o-vm-screenshot+a11y_tree",
-            "automata": gpt_4o(),
+            "logs_path": f"./logs/{MODEL}-vm-screenshot+a11y_tree",
+            "community": AllInOne(AGENT),
             "vm_path": os.environ["VM_PATH"],
             "headless": True,
             "obs_types": {OBS.screenshot, OBS.a11y_tree}
         },
         {
             "tasks_path": "./tasks/VM",
-            "logs_path": "./logs/gpt_4o-vm-set_of_marks",
-            "automata": gpt_4o(),
+            "logs_path": f"./logs/{MODEL}-vm-set_of_marks",
+            "community": AllInOne(AGENT),
             "vm_path": os.environ["VM_PATH"],
             "headless": True,
             "obs_types": {OBS.set_of_marks}
